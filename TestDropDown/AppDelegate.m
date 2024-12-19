7c7043bb3b1ff4ad152b61b7b3f9d121607b812e
zhenru wang <wang@dbtech.co.jp>
2024-12-19 22:20:21 +0900

    試験用のコード

--- TestDropDown/AppDelegate.m
+++ TestDropDown/AppDelegate.m
@@ -16,6 +16,61 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // 日期字符串数组
+            NSArray *dateStrings = @[
+                @"令和元年1月31日",
+                @"平成15年8月2日",
+                @"1987年6月27日",
+                @"1987年6月27日生"
+            ];
+            
+            // 正则表达式，支持和历与西历格式
+            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(?:(令和|平成|昭和|大正|明治)|(\\d+))((元|\\d+)?)年(\\d+)月(\\d+)日" options:0 error:nil];
+            
+            for (NSString *dateString in dateStrings) {
+                NSTextCheckingResult *match = [regex firstMatchInString:dateString options:0 range:NSMakeRange(0, dateString.length)];
+                
+                if (match) {
+                    NSString *era = nil;
+                    NSString *year = nil;
+                    NSString *month = nil;
+                    NSString *day = nil;
+                    
+                    // 提取年号或西历年
+                    if ([match rangeAtIndex:1].location != NSNotFound) { // 和历年号
+                        era = [dateString substringWithRange:[match rangeAtIndex:1]];
+                    } else if ([match rangeAtIndex:2].location != NSNotFound) { // 西历
+                        year = [dateString substringWithRange:[match rangeAtIndex:2]];
+                    }
+                    
+                    // 提取和历的年份或元年处理
+                    if ([match rangeAtIndex:3].location != NSNotFound && era) {
+                        NSString *yearPart = [dateString substringWithRange:[match rangeAtIndex:3]];
+                        if ([yearPart isEqualToString:@"元"]) {
+                            year = @"1"; // 元年处理为 1
+                        } else {
+                            year = yearPart;
+                        }
+                    }
+                    
+                    // 提取月和日
+                    if ([match rangeAtIndex:5].location != NSNotFound) {
+                        month = [dateString substringWithRange:[match rangeAtIndex:5]];
+                    }
+                    if ([match rangeAtIndex:6].location != NSNotFound) {
+                        day = [dateString substringWithRange:[match rangeAtIndex:6]];
+                    }
+                    
+                    // 打印提取结果
+                    if (era) {
+                        NSLog(@"Date String: %@, Era: %@, Year: %@, Month: %@, Day: %@", dateString, era, year, month, day);
+                    } else {
+                        NSLog(@"Date String: %@, Year: %@, Month: %@, Day: %@", dateString, year, month, day);
+                    }
+                } else {
+                    NSLog(@"未匹配日期字符串: %@", dateString);
+                }
+            }
     return YES;
 }
 
